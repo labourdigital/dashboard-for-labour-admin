035171d3c9b6cfb868cd67508ac2e75f42380fb4
Chris Bates-Keegan <chris@evansfinch.co.uk>
2017-05-17 17:43:59 +0100

    - REFACTORED: To use new campaign behaviours and d4l-campaign-img.

--- polymer-app/app/edit/src/views/twibbyn/d4l-twibbyn-details.js
+++ polymer-app/app/edit/src/views/twibbyn/d4l-twibbyn-details.js
@@ -1,32 +1,22 @@
 Polymer({
   is: 'd4l-twibbyn-details',
   behaviors: [
-    Polymer.D4LLogging
+    Polymer.D4LLogging,
+    Polymer.D4LCampaignDetails
   ],
   properties: {
     logLevel: {
       type: Number,
       value: 4
     },
-    auth: {
-      type: Object
-    },
-    db: {
+    metaDefault: {
       type: Object,
-      notify: true
-    },
-    twibbyn: {
-      type: Object,
-      observer: '__twibbynChanged',
-      notify: true
-    },
-    __statusDescription: {
-      type: String,
-      computed: '__computeStatusDescription(twibbyn.status)'
-    },
-    __published: {
-      type: Boolean,
-      computed: '__computePublished(twibbyn.status)'
+      value: function() {
+        return {
+          __populate__: true,
+          images: []
+        };
+      }
     },
     __openEditDialog: {
       type: Boolean,
@@ -41,10 +31,6 @@ Polymer({
         }
       }
     },
-    metadata: {
-      type: Object,
-      notify: true
-    },
     __uploadResponse: {
       type: Object,
       value: function() {
@@ -57,42 +43,16 @@ Polymer({
     '__onUploadResponse(__uploadResponse.response)'
   ],
 
-  __twibbynChanged: function(twibbyn) {
-    if (!twibbyn || !twibbyn.id) {
-      return;
-    }
-
-    let meta = this.get(['db.campaign.metadata', twibbyn.id]);
-    this.__debug('__twibbynChanged', twibbyn.id, meta);
-    if (!meta) {
-      const metaDefault = {
-        __populate__: true,
-        images: []
-      };
-      this.set(['db.campaign.metadata', twibbyn.id], metaDefault);
-    }
-    this.metadata = this.get(['db.campaign.metadata', twibbyn.id]);
-    this.linkPaths('metadata', `db.campaign.metadata.${twibbyn.id}`);
-  },
-
-  __publish: function() {
-    this.set('twibbyn.status', 'published');
-  },
-
-  __unpublish: function() {
-    this.set('twibbyn.status', 'pending');
-  },
-
   __editTwibbyn: function() {
-    this.__edit.title = this.get('twibbyn.name');
-    this.__edit.description = this.get('twibbyn.description');
+    this.__edit.title = this.get('campaign.name');
+    this.__edit.description = this.get('campaign.description');
     this.__openEditDialog = true;
   },
 
   __saveTwibbyn: function(ev) {
     this.__debug(ev.detail.item);
-    this.set('twibbyn.name', ev.detail.item.title);
-    this.set('twibbyn.description', ev.detail.item.description);
+    this.set('campaign.name', ev.detail.item.title);
+    this.set('campaign.description', ev.detail.item.description);
   },
 
   __removeImage: function(ev) {
@@ -111,17 +71,5 @@ Polymer({
     if (images.indexOf(response) === -1) {
       this.push('metadata.images', response);
     }
-  },
-
-  __computeStatusDescription(status) {
-    if (status !== 'published') {
-      return 'Not published yet.';
-    }
-
-    return 'Published';
-  },
-
-  __computePublished(status) {
-    return status === 'published';
   }
 });
--- polymer-app/app/edit/src/views/twibbyn/d4l-twibbyn.js
+++ polymer-app/app/edit/src/views/twibbyn/d4l-twibbyn.js
@@ -2,7 +2,8 @@ Polymer({
   is: 'd4l-twibbyn',
   behaviors: [
     Polymer.D4LLogging,
-    Polymer.D4LViewList
+    Polymer.D4LViewList,
+    Polymer.D4LCampaign
   ],
   properties: {
     logLevel: {
@@ -12,51 +13,24 @@ Polymer({
     auth: {
       type: Object
     },
+    type: {
+      type: String,
+      value: 'twibbyn'
+    },
     __pageTitle: {
       type: String,
       value: 'Twibbyn for Labour'
-    },
-    __twibbynIndices: {
-      type: Array,
-      value: function() { return []; }
     }
-  },
-  observers: [
-    '__twibbynSaved(db.campaign.data.*)'
-  ],
-
-  attached: function() {
   },
 
   __addTwibbyn: function() {
     this.push('db.campaign.data', {
       name: 'Twibbyn Campaign',
       type: 'twibbyn',
-      status: 'pending',
+      status: 'unpublished',
       ownerId: this.get('auth.user.id')
     });
-    this.__twibbynIndices.push(this.get('db.contract.data.length')-1);
-  },
-
-  __twibbynSaved: function(cr) {
-    this.__debug('__twibbynSaved', this.__twibbynIndices, cr);
-    if (this.__twibbynIndices.length === 0 || !cr.path) {
-      return;
-    }
-
-    let rex = /^db\.campaign\.data\.#(\d+).id$/;
-    let matches = rex.exec(cr.path);
-    if (!matches) {
-      return;
-    }
-
-    let index = this.__twibbynIndices.findIndex(i => i == matches[1]);
-    if (index === -1) {
-      this.__warn(`Invalid twibbyn index: ${matches[1]}`, this.__twibbynIndices);
-      return;
-    }
-
-    this.__contractIndices.splice(index, 1);
-  },
+    this.__twibbynIndices.push(this.get('db.campaign.data.length')-1);
+  }
 
 });
